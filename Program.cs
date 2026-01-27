082ee81714c5d3d28461aa534c641e950fa4b7b4
SweetieWind11 <stlopez@cocoschool.mx>
2026-01-27 13:20:42 -0600

    First Push

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL4;
+
+class Game : GameWindow
+{
+    private int _vao;
+    private int _vbo;
+    private int _ebo;
+    private Shader _shader;
+
+    public Game(GameWindowSettings gws, NativeWindowSettings nws) : base(gws, nws) { }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor(0.1f, 0.1f, 0.15f, 1f);
+        //4 vertices para generar un Quad
+        float[] vertices =
+        {
+            -0.5f,  0.5f, 1f, 0f, 0f,
+             0.5f,  0.5f, 0f, 1f, 0f,
+            -0.5f, -0.5f, 0f, 0f, 1f,
+             0.5f, -0.5f, 1f, 1f, 0f
+        };
+
+        uint [] indices = 
+        {
+            0, 2, 1,
+            2, 3, 1
+        };
+
+
+        _vao = GL.GenVertexArray();
+        _vbo = GL.GenBuffer();
+        _ebo = GL.GenBuffer();
+
+        GL.BindVertexArray(_vao);
+
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+        GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+       
+
+        //EBO
+        //Contiene dos indices de como leer los vertices. El EBO se guarda el VAO.
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
+        // VAO sabe cómo leer el VBO
+        // Atributo 1: Posiciones
+        GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+
+        // Atributo 2:
+        GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 2 * sizeof(float));
+        GL.EnableVertexAttribArray(1);
+        _shader = new Shader("Shaders/vertexColor.vert", "Shaders/vertexColor.frag");
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs e)
+    {
+        base.OnRenderFrame(e);
+
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        _shader.Use();
+        GL.BindVertexArray(_vao);
+        GL.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, 0);
+
+        SwapBuffers();
+    }
+
+    protected override void OnUnload()
+    {
+        base.OnUnload();
+        GL.DeleteBuffer(_ebo);
+        GL.DeleteBuffer(_vbo);
+        GL.DeleteVertexArray(_vao);
+        _shader.Dispose();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var gws = GameWindowSettings.Default;
+        var nws = new NativeWindowSettings
+        {
+            Title = "E03 - Square",
+            ClientSize = new Vector2i(800, 600),
+            API = ContextAPI.OpenGL,
+            APIVersion = new Version(3, 3),
+            Profile = ContextProfile.Core,
+            Flags = ContextFlags.ForwardCompatible
+        };
+
+        using var game = new Game(gws, nws);
+        game.Run();
+    }
+}
